c80f43035751dc7602e764bdf1bcf3fbc6109399
jjchann03 <95619907+jjchann03@users.noreply.github.com>
2024-03-18 11:08:24 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = use(express());
+
+app.post('/',(req,res)=>{
+    let data=req.body.data;
+    if(data){
+        let alphabets=[];
+        let odd=[];
+        let even=[]
+        for(let i=0;i<data.length;i++){
+            if(data[i]!=parseInt(data[i])) alphabets.add(data[i].toUpperCase());
+            else{
+                if(data[i]%2==0) even.add(data[i]);
+                else odd.add(data[i]);
+            }
+        }                                           
+        res.send({
+            "is_success": true,
+            "user_id": `${req.body.full_name}_${req.body.dob}`, 
+            "email": req.body.email,
+            "roll_number":req.body.roll_number,
+            "odd_numbers":odd,
+            "even_numbers":even,
+            "alphabets":alphabets
+        })
+    }
+})
